6de6e3d3f7a1e97e74c5edf9c83b9e1e4ae870b6
Egbetimmy <egbetiimmy@gmail.com>
2023-05-17 14:34:28 +0100

    handling preprocessing

--- model.py
+++ model.py
@@ -61,8 +61,11 @@ def _preprocess_data(data):
     df_train = feature_vector_df
     df_train = df_train.drop(['Unnamed: 0'], axis=1)
 
+    categorical_cols = ['Valencia_wind_deg', 'Seville_pressure']
+    df_train_encoded = pd.get_dummies(df_train, columns=categorical_cols)
+
     # create a copy
-    df_train_copy = df_train.copy(deep=True)
+    df_train_copy = df_train_encoded.copy(deep=True)
 
     # Replace the null values in Valencia_pressure with Madrid_pressure values on the same row.
     df_train_copy['Valencia_pressure'].fillna(df_train_copy['Madrid_pressure'], inplace=True)
@@ -71,18 +74,40 @@ def _preprocess_data(data):
     df_train_copy['time'] = pd.to_datetime(df_train_copy['time'])
     df_train_copy['year'] = df_train_copy['time'].dt.year
     df_train_copy['month'] = df_train_copy['time'].dt.month
+    df_train_copy['day'] = df_train_copy['time'].dt.day
     df_train_copy['Hours'] = df_train_copy['time'].dt.hour
 
     # Drop unnecessary columns and fill missing values with 0
     df_train_copy.drop(['time'], axis='columns', inplace=True)
     df_train_copy.fillna(0, inplace=True)
 
-    features = ['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
-                'Valencia_wind_speed', 'Seville_humidity',
-                'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed',
-                'Seville_clouds_all', 'Bilbao_wind_deg',
-                'Barcelona_wind_speed', 'Barcelona_wind_deg', 'Madrid_clouds_all', 'Seville_wind_speed',
-                'Barcelona_rain_1h', 'Seville_pressure', 'Bilbao_snow_3h']
+    features = ['Madrid_wind_speed', 'Bilbao_rain_1h',
+                'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
+                'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
+                'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
+                'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h',
+                'Seville_rain_1h', 'Bilbao_snow_3h', 'Barcelona_pressure',
+                'Seville_rain_3h', 'Madrid_rain_1h', 'Barcelona_rain_3h',
+                'Valencia_snow_3h',
+                'Bilbao_pressure', 'Valencia_pressure', 'Madrid_pressure',
+                'Valencia_temp', 'Seville_temp',
+                'Valencia_humidity', 'Barcelona_temp', 'Bilbao_temp',
+                'Madrid_temp', 'Hours', 'Valencia_wind_deg_level_10', 'Valencia_wind_deg_level_2',
+                'Valencia_wind_deg_level_3', 'Valencia_wind_deg_level_4',
+                'Valencia_wind_deg_level_5', 'Valencia_wind_deg_level_6',
+                'Valencia_wind_deg_level_7', 'Valencia_wind_deg_level_8',
+                'Valencia_wind_deg_level_9', 'Seville_pressure_sp10',
+                'Seville_pressure_sp11', 'Seville_pressure_sp12',
+                'Seville_pressure_sp13', 'Seville_pressure_sp14',
+                'Seville_pressure_sp15', 'Seville_pressure_sp16',
+                'Seville_pressure_sp17', 'Seville_pressure_sp18',
+                'Seville_pressure_sp19', 'Seville_pressure_sp2',
+                'Seville_pressure_sp20', 'Seville_pressure_sp21',
+                'Seville_pressure_sp22', 'Seville_pressure_sp23',
+                'Seville_pressure_sp24', 'Seville_pressure_sp25',
+                'Seville_pressure_sp3', 'Seville_pressure_sp4', 'Seville_pressure_sp5',
+                'Seville_pressure_sp6', 'Seville_pressure_sp7', 'Seville_pressure_sp8',
+                'Seville_pressure_sp9', 'month', 'day']
 
     # ----------- Replace this code with your own preprocessing steps --------
     predict_vector = feature_vector_df[features]
